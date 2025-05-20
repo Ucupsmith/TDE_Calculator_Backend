c413480b7c0b2e03cc26c0669f2f74c19a81bb62
Langgeng <akilnauffancozz@gmail.com>
2025-05-20 15:12:03 +0700

    docs : changes on env local

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
-import express from "express";
-import * as path from "path";
-import dotenv from "dotenv";
-import { userRoutes } from "./routes/UserRoutes.js";
-import { profileRoutes } from "./routes/profileRoutes.js";
-import { mealPlanRoutes } from "./routes/mealPlanRoutes.js";
-import { tdeeRoutes } from "./routes/tdeeRoutes.js";
-import { notificationRoutes } from "./routes/notificationRoutes.js";
+import express from 'express';
+import * as path from 'path';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { userRoutes } from './routes/UserRoutes.js';
+import { profileRoutes } from './routes/profileRoutes.js';
+import { mealPlanRoutes } from './routes/mealPlanRoutes.js';
+import { tdeeRoutes } from './routes/tdeeRoutes.js';
+import { notificationRoutes } from './routes/notificationRoutes.js';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware for parsing JSON request body
 app.use(express.json());
+app.use(cors());
 
 // Root route
-app.get("/", (req, res) => {
-  res.status(200).send("welcome to TDEE calculator API");
+app.get('/', (req, res) => {
+  res.status(200).send('welcome to TDEE calculator API');
 });
 
 // API routes
-app.use("/user/v1/users", userRoutes);
-app.use("/user/v1/profiles", profileRoutes);
-app.use("/user/v1/meal-plans", mealPlanRoutes);
-app.use("/user/v1/articles", articleRoutes);
-app.use("/user/v1/tdee", tdeeRoutes);
-app.use("/user/v1/notifications", notificationRoutes);
+app.use('/user/v1/users', userRoutes);
+app.use('/user/v1/profiles', profileRoutes);
+app.use('/user/v1/meal-plans', mealPlanRoutes);
+// app.use("/user/v1/articles", articleRoutes);
+app.use('/user/v1/tdee', tdeeRoutes);
+app.use('/user/v1/notifications', notificationRoutes);
 
 // Error handler middleware (should come after routes)
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send("Something broke! Please Try Again Later! ");
+  res.status(500).send('Something broke! Please Try Again Later! ');
 });
 
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
 });
-
-
