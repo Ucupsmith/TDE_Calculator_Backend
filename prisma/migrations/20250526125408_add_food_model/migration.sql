13adc6d0dfc28d429f76791b8c126c72cfd054aa
aryariyaanto <tokutokuriyanto@gmail.com>
2025-05-26 20:15:23 +0700

    feature : Mealplan update + histoy mealplan -arya

---
+++ prisma/migrations/20250526125408_add_food_model/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Food` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `calories` INTEGER NOT NULL,
+    `unit` VARCHAR(191) NOT NULL,
+    `imageUrl` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
