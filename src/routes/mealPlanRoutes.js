1effc2653d952bd7a91bdc275587927f02458633
aryariyaanto <tokutokuriyanto@gmail.com>
2025-06-01 21:27:43 +0700

    Feature : Mealplan goals -arya

--- src/routes/mealPlanRoutes.js
+++ src/routes/mealPlanRoutes.js
@@ -6,7 +6,8 @@ import {
   getLatestSelection, 
   getCurrentDayCaloriesController,
   getMealPlanHistory,
-  updateMealSelection
+  updateMealSelection,
+  getMealPlanSummary
 } from '../controllers/UserMealSelectionController.js';
 
 // Create meal plan
@@ -26,5 +27,8 @@ router.get('/history', getMealPlanHistory);
 
 // Update meal selection
 router.put('/selections/:id', updateMealSelection);
+
+// Get meal plan summary
+router.get('/summary', getMealPlanSummary);
 
 export default router; 
