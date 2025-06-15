39834b0c24c438854648a07184f3666f0d75e45b
aryariyaanto <tokutokuriyanto@gmail.com>
2025-06-15 10:37:57 +0700

    update tdee calcualation activity-arya

--- src/models/TdeeModel.js
+++ src/models/TdeeModel.js
@@ -45,11 +45,11 @@ export function calculateBMRMifflinStJeor(gender, weight, height, age) {
 // TDEE calculation (Katch-McArdle)
 export function calculateTDEE(bmr, activityLevel, goal) {
   const factors = {
-    sedentary: 1.2,
-    slightly_active: 1.375,
-    moderately_active: 1.55,
-    very_active: 1.725,
-    extra_active: 1.9
+    'Sedentary': 1.2,
+    'Lightly Active': 1.375,
+    'Moderately Active': 1.55,
+    'Very Active': 1.725,
+    'Extra Active': 1.9
   };
 
   let baseTDEE = bmr * (factors[activityLevel] || 1.2);
